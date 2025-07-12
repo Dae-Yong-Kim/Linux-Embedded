16bcb1716c23052c4780e60bc451b0f170934aa8
Dae-Yong-Kim <youbinlove607@gmail.com>
2025-07-12 23:12:59 +0900

    how to add system call & Debug

---
+++ Week04/sizeof_int.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	printf("sizeof(int) = %d\n", sizeof(int));
+	printf("sizeof(long) = %d\n", sizeof(long));
+	printf("sizeof(long long) = %d\n", sizeof(long long));
+	printf("sizeof(int64_t) = %d\n", sizeof(int64_t));
+}
